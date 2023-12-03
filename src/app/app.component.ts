c42ef7bf1592a9c00004d89028884aff39a6306b
volo4dev <98762181+volo4dev@users.noreply.github.com>
2023-12-03 21:34:31 +0200

    Create todo-app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,16 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 
+import { HeaderComponent } from './core/header/header.component';
+import { ContentComponent } from './core/content/content.component';
+import { TodoComponent } from './features/todo/todo.component';
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, HeaderComponent, ContentComponent, TodoComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  title = 'todo-list-17';
 }
